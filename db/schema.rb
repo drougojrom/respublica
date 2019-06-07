a61e7f08733a5f8a5d2e8822cf751a0029f16930
Roman Ustiantcev <liberrom.hse@gmail.com>
2019-06-07 16:13:31 +0200

    product with tests - added

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190607135108) do
+ActiveRecord::Schema.define(version: 20190607140823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "articul"
+    t.string "title"
+    t.float "weight"
+    t.float "size"
+    t.string "color"
+    t.float "price"
+    t.integer "amount"
+    t.bigint "shop_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shop_id"], name: "index_products_on_shop_id"
+  end
 
   create_table "shops", force: :cascade do |t|
     t.string "title"
@@ -24,4 +38,5 @@ ActiveRecord::Schema.define(version: 20190607135108) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "products", "shops"
 end
