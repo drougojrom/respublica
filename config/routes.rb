5da3d64574beabbd5ec6817d4b2fbebd098bc04e
Roman Ustiantcev <liberrom.hse@gmail.com>
2019-06-13 00:39:33 +0200

    rename, routes clean

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :shops do
+  resources :shops, only: %i[index new show create destroy] do
     resources :products, only: %i[show new create]
   end
 
-  get 'search_products', to: 'products#filter'
+  get 'filter_products', to: 'products#filter'
 
   root to: 'shops#index'
 end
