d8cdd05583111c4872b477107596ec84bc71c2c5
Roman Ustiantcev <liberrom.hse@gmail.com>
2019-06-12 15:47:10 +0200

    tests for product creation - added

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -22,11 +22,28 @@ RSpec.describe ProductsController, type: :controller do
 
   describe 'POST #create' do
     context 'with valid attributes' do
+      it 'saves a new product into db' do
+        expect { post :create, params: { shop_id: shop.id, product: attributes_for(:product) },
+                      format: :js}.to change(Product, :count).by(1)
+      end
 
+      it 'redirects to product' do
+        post :create, params: { shop_id: shop.id, product: attributes_for(:product) }
+        product = Product.last
+        expect(response).to redirect_to shop_product_path(shop_id: shop, id: product.id)
+      end
     end
 
     context 'with invalid attributes' do
+      it 'does not save a new product into db' do
+        expect { post :create, params: { shop_id: shop.id, product: attributes_for(:product, :invalid) },
+                      format: :js}.to_not change(Product, :count)
+      end
 
+      it 'does not redirect to product' do
+        post :create, params: { shop_id: shop.id, product: attributes_for(:product, :invalid) }, format: :js
+        expect(response).to render_template :create
+      end
     end
   end
 end
