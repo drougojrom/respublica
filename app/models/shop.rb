b0902f3ad407a3147c1a6b5ed0f7acadcf6e0dc9
Roman Ustiantcev <liberrom.hse@gmail.com>
2019-06-13 00:45:00 +0200

    delete method - added

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,6 +1,6 @@
 class Shop < ApplicationRecord
   validates :title, :address, :city, presence: true
 
-  has_many :product_availabilities
+  has_many :product_availabilities, dependent: :destroy
   has_many :products, through: :product_availabilities
 end
